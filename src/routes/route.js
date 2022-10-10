a0d61fc6fd7a118fdcc69c1a8fb50b05813ba56f
Omprakash <omdhanurkar444@gmail.com>
2022-10-11 01:02:09 +0530

    route

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controller/userController')
 
 
-//=======================test Api========================
+//=======================test Api=========================
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
