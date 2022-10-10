b6db8f9bffcd8b2c5cd7cbdf42e84b8bdc3afa85
Omprakash <omdhanurkar444@gmail.com>
2022-10-11 00:59:58 +0530

    validator

--- src/utility/validator.js
+++ src/utility/validator.js
@@ -1,28 +1,34 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
 const isValid = function (value) {
     if (typeof value === "undefined" || value === null) return false;
     if (typeof value === "string" && value.trim().length === 0) return false;
+    if (!value) return false
     return true;
 };
 
 
-const isValidObjectId = function (ObjectId) {
-    return mongoose.Types.ObjectId.isValid(ObjectId)
-}
+//------------------------------- Fname regex --------------------------------------------//
 
+const isValidFname = function (name) {
+    return /^[a-zA-Z .]{2,30}$/.test(name);
+};
+//------------------------------- Lname regex --------------------------------------------//
 
-//------------------------------- password regex ------------------------------------------//
-
-const isValidPassword = function (password) {
-    return (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/.test(password))
-}
-
+const isValidLname = function (name) {
+    return /^[a-zA-Z ]{2,30}$/.test(name);
+};
 //------------------------------- email regex --------------------------------------------//
 
 const isVAlidEmail = function (email) {
     return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email)
 }
+//------------------------------- password regex --------------------------------------------//
+
+
+const isValidPassword = function (pass) {
+    return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(pass);
+};
 
 //------------------------------- phone regex --------------------------------------------//
 
@@ -30,4 +36,26 @@ const isValidPhone = function (phone) {
     return (/^[6789]\d{9}$/).test(phone)
 
 }
+//------------------------------- street regex --------------------------------------------//
 
+const isValidStreet = function (value) {
+    return /^[a-zA-Z0-9 -.,]{2,30}$/.test(value);
+};
+//------------------------------- pincode regex --------------------------------------------//
+
+const isValidPincode = function (num) {
+    return /^[1-9][0-9]{5}$/.test(num);
+};
+
+
+module.exports = {
+    isValid,
+    isValidFname,
+    isValidLname,
+    isValidStreet,
+    isValidPassword,
+    isVAlidEmail,
+    isValidPhone,
+    isValidPincode
+
+}
