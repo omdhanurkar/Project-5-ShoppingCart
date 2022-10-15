c7785385d5351de8b2012ee3946a0f9d1e3ee527
Omprakash <omdhanurkar444@gmail.com>
2022-10-15 19:28:58 +0530

    AWS S3

---
+++ src/controller/awsController.js
@@ -0,0 +1,29 @@
+const aws = require("aws-sdk")
+
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+});
+let uploadFile = async (file) => {
+    return new Promise(function (resolve, reject) {
+        // this function will upload file to aws and return the link
+        let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket",
+            Key: "abc/" + file.originalname,
+            Body: file.buffer
+        }
+        s3.upload(uploadParams, function (err, data) {
+            if (err) {
+                return reject({ "error": err.message })
+            }
+            console.log("file uploaded succesfully")
+            return resolve(data.Location)
+        })
+    })
+}
+
+module.exports={uploadFile}
